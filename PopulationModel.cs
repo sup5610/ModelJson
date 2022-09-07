8ac34e47ff3b31d26d8053926d51e1ba4bd47b01
M <supsune2@gmail.com>
2022-09-07 23:53:26 +0100

    Add files via upload

---
+++ PopulationModel.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//namespace ModelThatJSONCorrectVersion
+//{
+//    class PopulationModel
+//    {
+//        public _PreyList[]? preyList { get; set; }
+//        public _PredList[]? predList { get; set; }
+//    }
+
+//    public class _PreyList
+//    {
+//        public _Rabbit[]? rabbit { get; set; }
+//        public _Deer[]? deer { get; set; }
+//    }
+//    public class _Rabbit
+//    {
+//        public string Species { get; set; } = string.Empty; // declare Species as an empty string
+//        public int Id { get; set; }
+//        public int Attack { get; set; }
+//        public int Health { get; set; }
+//        public int Speed { get; set; }
+//    }
+//    public class _Deer
+//    {
+//        public string Species { get; set; } = string.Empty;
+//        public int Id { get; set; }
+//        public int Attack { get; set; }
+//        public int Health { get; set; }
+//        public int Speed { get; set; }
+
+//    }
+
+
+//    public class _PredList
+//    {
+//        public _Wolf[]? wolf { get; set; }
+//        public _Jaguar[]? jaguar { get; set; }
+//    }
+
+//    public class _Wolf
+//    {
+//        public string Species { get; set; } = string.Empty;
+//        public int Id { get; set; }
+//        public int Attack { get; set; }
+//        public int Health { get; set; }
+//        public int Speed { get; set; }
+//    }
+//    public class _Jaguar
+//    {
+//        public string Species { get; set; } = string.Empty;
+//        public int Id { get; set; }
+//        public int Attack { get; set; }
+//        public int Health { get; set; }
+//        public int Speed { get; set; }
+//    }
+//}
+
+
+namespace ModelThatJSONCorrectVersion
+{
+    public class PopulationModel
+    {
+        public _PreyList[]? preyList { get; set; }
+        public _PredList[]? predList { get; set; }
+    }
+    public class _PredList
+    {
+        public _Wolves[]? wolves { get; set; }
+        public _Jaguar[]? jaguars { get; set; }
+    }
+
+    public class _Jaguar
+    {
+        public int id { get; set; }
+        public int attack { get; set; }
+        public int health { get; set; }
+        public int speed { get; set; }
+        public string guid { get; set; } = string.Empty;
+    }
+    public class _Wolves
+    {
+        public int id { get; set; }
+        public int attack { get; set; }
+        public int health { get; set; }
+        public int speed { get; set; }
+        public string guid { get; set; } = string.Empty;
+    }
+    public class _PreyList
+    {
+        public _Rabbits[]? rabbits { get; set; }
+        public _Deer[]? deer { get; set; }
+    }
+    public class _Rabbits
+    {
+        public int id { get; set; }
+        public int attack { get; set; }
+        public int health { get; set; }
+        public int speed { get; set; }
+        public string guid { get; set; } = string.Empty;
+    }
+    public class _Deer
+    {
+        public int id { get; set; }
+        public int attack { get; set; }
+        public int health { get; set; }
+        public int speed { get; set; }
+        public string guid { get; set; } = string.Empty;
+    }
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace ModelThatJSONCorrectVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"C:\Users\supsu\OneDrive\School Python\Project\Simulation Building\Animal Classes Py\startingPopulation.json";
+
+            string jsonText = File.ReadAllText(fileName);
+
+            var data = Newtonsoft.Json.JsonConvert.DeserializeObject<PopulationModel>(jsonText);// already have the object layout set up and this will assign from json to the pre existing object
+            //Console.WriteLine(data);
+            //PopulationModel data = Newtonsoft.Json.JsonConvert.DeserializeObject<PopulationModel>(jsonText);// already have the object layout set up and this will assign from json to the pre existing object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Console.WriteLine(data.predList);
+            //Console.WriteLine(data.predList[0]);
+            //Console.WriteLine(data.predList[0].wolf);
+            //Console.WriteLine(data.predList[0].wolf[0]);
+            //Console.WriteLine(data.predList[0].wolf[0].Attack);
+            //Console.WriteLine(data.predList[0].jaguar);
+
+
+            //var badKeys = data.predList[0].Where(pair => pair.value.count == 0)
+            //    .Select(pair => pair.Key)
+            //    .ToList();
+
+
+        }
+    }
+
+    
+}
